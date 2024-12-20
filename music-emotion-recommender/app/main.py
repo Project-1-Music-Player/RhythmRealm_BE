0bfa50526081b15a2533d3654d28c969b609a465
dedyoc <huyhoaius@gmail.com>
2024-12-20 11:42:32 +0700

    feat: add CORS support

--- music-emotion-recommender/app/main.py
+++ music-emotion-recommender/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 from app.api.routes import router
 from app.db.database import init_db
+import os
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Music Recommendation API")
+origins = [
+    "http://localhost:3001",
+    "http://localhost:8000",  
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 # Include routers
 app.include_router(router, prefix="/api")
